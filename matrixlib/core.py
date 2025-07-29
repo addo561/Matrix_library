73c6349236849278d6e5f612a73230c15b6d1344
addo561 <korlilarryaddo43@gmail.com>
2025-07-29 23:25:55 +0000

    create matrix class

--- matrixlib/core.py
+++ matrixlib/core.py
@@ -1,1 +1,33 @@
 # Matrix class and basic operations
+# focuses on 1d,2d
+from typing_extensions import List
+
+
+class MatrixCreation:
+    @staticmethod
+    def Two_D(inputs: List|tuple):
+        pass
+    
+    @staticmethod
+    def One_D():
+        pass
+        
+    @staticmethod
+    def zeros():
+        pass
+        
+    @staticmethod    
+    def ones():
+        pass
+    
+    @staticmethod    
+    def Identity():
+        pass
+    
+    @staticmethod
+    def rand():
+        pass
+    
+    @staticmethod
+    def randint():
+        pass
