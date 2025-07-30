abd4b864a0b70612cb4222964f440d494772c07d
addo561 <korlilarryaddo43@gmail.com>
2025-07-30 00:58:31 +0000

    matrix 2d and 1d done

--- matrixlib/core.py
+++ matrixlib/core.py
@@ -5,33 +5,39 @@ from typing_extensions import List,Tuple
 
 class MatrixCreation:
     @staticmethod
-    def matrix_2d(inputs: List|Tuple,Type:str):
-        if type(inputs) == tuple and  Type == '1s':
-            L =  [ 1 for _ in  range(inputs[1]) for _ in  range(inputs[0])]
+    def matrix_2d(inputs: List|Tuple,Type=None):
+        if type(inputs) == tuple : #for zeros  and ones
+            L =  [ [Type for _ in  range(inputs[1])] for _ in  range(inputs[0])]
             return  L
         return  inputs
 
 
     @staticmethod
-    def matrix_1d(t_L:List|int,Type:str):
-        #for ones
-        if type(t_L)==int and Type == '1s':
-            L =  [1 for _ in range(t_L)]
+    def matrix_1d(t_L:List|int,Type=None):
+        #for ones and  zeros
+        if type(t_L)==int:
+            L =  [Type for _ in range(t_L)]
             return L
         return t_L
 
     @staticmethod
-    def zeros():
-        pass
+    def zeros(n1=None,n2=None):
+        #takes input  as two integers
+        zeros = []
+        if n1 and not  n2:
+            zeros = MatrixCreation.matrix_1d(n1,Type=0)# O for zeros
+        elif  n1 and n2 :
+            zeros =  MatrixCreation.matrix_2d((n1,n2),Type=0)
+        return  zeros
 
     @staticmethod
     def ones(n1=None,n2=None):
         #takes input  as two integers
         ones = []
-        if n1:
-            ones = MatrixCreation.matrix_1d(n1,Type='1s')# 1 for ones
+        if n1 and not  n2:
+            ones = MatrixCreation.matrix_1d(n1,Type=1)# 1 for ones
         elif  n1 and n2 :
-            ones =  MatrixCreation.matrix_2d((n1,n2),Type='1s')
+            ones =  MatrixCreation.matrix_2d((n1,n2),Type=1)
         return  ones
 
     @staticmethod
@@ -46,5 +52,6 @@ class MatrixCreation:
     def randint():
         pass
 
-m = MatrixCreation.ones()
+#just for checking  whiles coding,wrote  tests  later
+m = MatrixCreation.matrix_2d([[1],[2],[3]])
 print(m)
