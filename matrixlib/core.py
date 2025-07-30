0e14d7bf37ea0d859f69455bce9b7c1f8ab20d0d
addo561 <korlilarryaddo43@gmail.com>
2025-07-30 00:42:14 +0000

    added string to indicate operations

--- matrixlib/core.py
+++ matrixlib/core.py
@@ -5,12 +5,15 @@ from typing_extensions import List,Tuple
 
 class MatrixCreation:
     @staticmethod
-    def matrix_2d(inputs: List|Tuple):
-        pass
+    def matrix_2d(inputs: List|Tuple,Type:str):
+        if type(inputs) == tuple and  Type == 'O':
+
+
 
     @staticmethod
-    def matrix_1d(t_L:List|int):
-        if type(t_L)==int:
+    def matrix_1d(t_L:List|int,Type:str):
+        #for zeros
+        if type(t_L)==int and Type == 'O':
             L =  [0 for _ in range(t_L)]
             return L
         return t_L
@@ -21,14 +24,12 @@ class MatrixCreation:
 
     @staticmethod
     def ones(n1=None,n2=None):
-        #takes input  as two integera
+        #takes input  as two integers
         ones = []
-        if n1 and not n2:
-            ones = MatrixCreation.matrix_1d(n1)
-        elif n2 and not n1:
-            ones = MatrixCreation.matrix_1d(n2)
+        if n1:
+            ones = MatrixCreation.matrix_1d(n1,Type='O')# O for ones
         elif  n1 and n2 :
-            ones =  MatrixCreation.matrix_2d((n1,n2))
+            ones =  MatrixCreation.matrix_2d((n1,n2),Type='O')
         return  ones
 
     @staticmethod
@@ -42,3 +43,6 @@ class MatrixCreation:
     @staticmethod
     def randint():
         pass
+
+m = MatrixCreation.ones()
+print(m)
