cc9e0bad8ce549a025033b52fafc562d2df53157
addo561 <korlilarryaddo43@gmail.com>
2025-07-30 11:47:19 +0000

    created Identity function and updated 2d matrix function

--- matrixlib/core.py
+++ matrixlib/core.py
@@ -21,14 +21,21 @@ class MatrixCreation:
                             L[i][j] = Type[idx]
                             idx += 1
                 return L
+        if function  ==  'I':
+            matrix  = [ [0 for _ in  range(inputs[1])] for _ in  range(inputs[0])]
+            for i in range(inputs[0]):
+                for j  in range(inputs[1]):
+                    if i ==j:
+                        matrix[i][j]=1
+            return matrix
 
         return  inputs #returns initialized list
 
 
     @staticmethod
-    def matrix_1d(t_L:Union[List,int],Type=None,):
+    def matrix_1d(t_L:Union[List,int],function=None,Type=None,):
         #for ones and  zeros
-        if isinstance(t_L,int):
+        if isinstance(t_L,int) and function is None:
             L =  [Type for _ in range(t_L)]
             return L
         return t_L #returns initialized list
@@ -40,7 +47,7 @@ class MatrixCreation:
         if n1 and not  n2:
             zeros = MatrixCreation.matrix_1d(n1,Type=0)# O for zeros
         elif  n1 and n2 :
-            zeros =  MatrixCreation.matrix_2d((n1,n2),function=None,Type=0)
+            zeros =  MatrixCreation.matrix_2d((n1,n2),Type=0)
         return  zeros
 
     @staticmethod
@@ -50,12 +57,16 @@ class MatrixCreation:
         if n1 and not  n2:
             ones = MatrixCreation.matrix_1d(n1,Type=1)# 1 for ones
         elif  n1 and n2 :
-            ones =  MatrixCreation.matrix_2d((n1,n2),function=None,Type=1)
+            ones =  MatrixCreation.matrix_2d((n1,n2),Type=1)
         return  ones
 
     @staticmethod
-    def Identity():
-        pass
+    def Identity(x,y):
+        I = []
+        I = MatrixCreation.matrix_2d((x,y),function='I',Type=None)
+        return I
+
+
 
     @staticmethod
     def normal(low,high,size:Tuple):
