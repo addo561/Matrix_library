bf55cc09e5e9940a3bfb25ff75429fb237dcd0af
addo561 <korlilarryaddo43@gmail.com>
2025-07-31 12:02:45 +0000

    adding exceptions

--- matrixlib/core.py
+++ matrixlib/core.py
@@ -8,37 +8,42 @@ import  random
 class MatrixCreation:
     @staticmethod
     def matrix_2d(inputs: Union[List,Tuple],function=None,Type=None):
-        if isinstance(inputs,tuple ) and function is None: #for zeros  and ones
-            L =  [ [Type for _ in  range(inputs[1])] for _ in  range(inputs[0])]
-            return  L
+        try:
+            if isinstance(inputs,tuple ) and function is None: #for zeros  and ones
+                L =  [ [Type for _ in  range(inputs[1])] for _ in  range(inputs[0])]
+                return  L
 
-        if  function =='normal':
-            L =  [ [0 for _ in  range(inputs[1])] for _ in  range(inputs[0])]
-            if Type:
-                idx =  0
-                for i in range(len(L)):
-                    for j in range(len(L[0])):
-                            L[i][j] = Type[idx]
-                            idx += 1
-                return L
-        if function  ==  'I':
-            matrix  = [ [0 for _ in  range(inputs[1])] for _ in  range(inputs[0])]
-            for i in range(inputs[0]):
-                for j  in range(inputs[1]):
-                    if i ==j:
-                        matrix[i][j]=1
-            return matrix
-
-        return  inputs #returns initialized list
+            if  function =='normal':
+                L =  [ [0 for _ in  range(inputs[1])] for _ in  range(inputs[0])]
+                if Type:
+                    idx =  0
+                    for i in range(len(L)):
+                        for j in range(len(L[0])):
+                                L[i][j] = Type[idx]
+                                idx += 1
+                    return L
+            if function  ==  'I':
+                matrix  = [ [0 for _ in  range(inputs[1])] for _ in  range(inputs[0])]
+                for i in range(inputs[0]):
+                    for j  in range(inputs[1]):
+                        if i ==j:
+                            matrix[i][j]=1
+                return matrix
+        except Exception  as e:
+            print(f'Error:{e}')
+            return  inputs #returns initialized list
 
 
     @staticmethod
     def matrix_1d(t_L:Union[List,int],function=None,Type=None,):
         #for ones and  zeros
-        if isinstance(t_L,int) and function is None:
-            L =  [Type for _ in range(t_L)]
-            return L
-        return t_L #returns initialized list
+        try:
+            if isinstance(t_L,int) and function is None:
+                L =  [Type for _ in range(t_L)]
+                return L
+        except Exception as e:
+            print(f'Error: {e}')
+            return t_L #returns initialized list
 
     @staticmethod
     def zeros(n1=None,n2=None):
@@ -62,7 +67,6 @@ class MatrixCreation:
 
     @staticmethod
     def Identity(x,y):
-        I = []
         I = MatrixCreation.matrix_2d((x,y),function='I',Type=None)
         return I
 
