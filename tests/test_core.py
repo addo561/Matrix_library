be40b744bab920c69120c2d296f3e16796f8219b
addo561 <korlilarryaddo43@gmail.com>
2025-08-01 23:19:47 +0000

    tests on 2dmatrix function

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,1 +1,16 @@
 # Tests for core matrix operations
+import pytest
+from matrixlib import core
+
+#functions  test
+matrix_2d = core.MatrixCreation.matrix_2d
+matrix_1d = core.MatrixCreation.matrix_1d
+zeros = core.MatrixCreation.zeros
+ones = core.MatrixCreation.ones
+randint =  core.MatrixCreation.randint
+normal =  core.MatrixCreation.normal
+
+#simple testing ()
+@pytest.mark.parametrize('test_input,expected',[([[2,2]],[[2,2]]), ([2,2],ValueError) ])
+def test_eval(test_input, expected):
+    assert matrix_2d(test_input) == expected
